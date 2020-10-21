9a75a358c5aa909a189304ae8e68e4ab7afc5b1b
Takeep <60165073+Takeep@users.noreply.github.com>
2020-10-20 22:06:40 -0400

    Create Main_file.py
    This is more or less the founding page file of all the files i will be updating the code as i go along.

---
+++ Main_file.py
@@ -0,0 +1,11 @@
+import pygame
+import quit
+import character
+import movement
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500))
+pygame.display.set_caption('Super mario')
+
+pygame.display.update()
